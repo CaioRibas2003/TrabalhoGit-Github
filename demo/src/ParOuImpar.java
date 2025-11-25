04ad932d1a45c853af420f485bd016e111dd54a9
Caio Ribas <caioribasemm@gmail.com>
2025-11-25 20:34:36 -0300

    Criar classe, atributo, e scanner

---
+++ demo/src/ParOuImpar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//Criacao classe: Caio
+public class ParOuImpar {
+
+    //Criacao atributo: Caio
+    int numero;
+
+    //Criacao scanner: Caio
+    Scanner sc = new Scanner(System.in);
+
+}
