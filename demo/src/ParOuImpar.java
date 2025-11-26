230cce882fa34ea9f2249edd6779bd7cd71cbe4f
Brian Costa <brian.costa@edu.unifil.br>
2025-11-25 21:06:54 -0300

    Add entrada de dados e psvm

--- demo/src/ParOuImpar.java
+++ demo/src/ParOuImpar.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 
 //Criacao classe: Caio
 public class ParOuImpar {
+    static void main() {
+        //Criacao atributo: Caio
+        int numero;
 
-    //Criacao atributo: Caio
-    int numero;
+        //Criacao scanner: Caio
+        Scanner sc = new Scanner(System.in);
 
-    //Criacao scanner: Caio
-    Scanner sc = new Scanner(System.in);
+        //Criação da entrada de dados: Brian
+        System.out.println("Digite o número a ser verificado: ");
+        numero = sc.nextInt();
+    }
 
 }
